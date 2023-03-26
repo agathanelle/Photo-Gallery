f97b1eaf336c229dd433ce9bc73e7b529dc51cce
agathanelle <agatageb@gmail.com>
2023-03-26 20:14:19 +0300

    fixes

--- website/src/common/consts.ts
+++ website/src/common/consts.ts
@@ -1,4 +1,4 @@
 export const PexelsAPIUrl = "https://api.pexels.com";
-export const PexelsAPIKey = process.env.APIKEY;
+export const PexelsAPIKey = 'ZgRu4ZmqXADuks9QidbPiCN6OoMIurqO6j7u1a3ZSiINaKIRrhtJM93M';
 export const queryValue = 'nature';
 export const perPageValue = 15;
